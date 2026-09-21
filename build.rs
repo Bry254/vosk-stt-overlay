04854c3fa8136f55a51478ac537292b00c128b15
Bry254 <brayan254lopez154@gmail.com>
2026-09-20 20:02:33 -0500

    modified:   .gitignore 	modified:   build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=libs");
+    println!("cargo:rustc-link-search=native=lib");
+    println!("cargo:rustc-link-lib=dylib=vosk");
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 }
